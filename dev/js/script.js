415b15caf9b21a5ea0703f9ec4e6ba9d955f55e3
Adam Farah <adamfarah@me.com>
2015-04-03 11:36:01 +0100

    Re-added CNAME

---
+++ dev/js/script.js
@@ -0,0 +1,8 @@
+// $(window).scroll(function() {
+// 	var $scrollTop = $(window).scrollTop();
+// 	if ($scrollTop >= 50) {
+// 		$("#headerlogo").addClass("shrunk");
+// 	} else {	
+// 		$("#headerlogo").removeClass("shrunk");
+// 	}
+// });
